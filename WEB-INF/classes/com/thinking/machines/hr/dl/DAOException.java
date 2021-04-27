6550910ba04688a305b512eb665a4eb337e76dc9
unknown <harshit012>
2021-04-27 01:21:02 -0400

    First commit

--- WEB-INF/classes/com/thinking/machines/hr/dl/DAOException.java
+++
@@ -1,8 +0,0 @@
-package com.thinking.machines.hr.dl;
-public class DAOException extends Exception
-{
-public DAOException(String message)
-{
-super(message);
-}
-}
--- WEB-INF/classes/com/thinking/machines/hr/servlets/Customer.java
+++
@@ -1,7 +0,0 @@
-package com.thinking.machines.hr.servlets;
-public class Customer implements java.io.Serializable
-{
-public String firstName;
-public String lastName;
-public int age;
-}
